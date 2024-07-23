d95be62141be4911280dfd059cc0bd82da72699b
Ela-El-maker <feloela@gmail.com>
2024-07-23 19:50:57 +0300

    Bug Report : CSS bug -> frontend

--- resources/views/frontend/home_all/home_about.blade.php
+++ resources/views/frontend/home_all/home_about.blade.php
@@ -9,32 +9,31 @@
 
         <div class="row gy-4">
 
-            <div class="col-lg-6 position-relative align-self-start order-lg-last order-first"
-                data-aos="fade-up" data-aos-delay="200">
-                <img src="{{asset($aboutPage->foreground_image)}}" class="img-fluid" alt="">
-                <a href="{{$aboutPage->video_url}}"
-                    class="glightbox pulsating-play-btn"></a>
+            <div class="col-lg-6 position-relative align-self-start order-lg-last order-first" data-aos="fade-up"
+                data-aos-delay="200">
+                <img src="{{ asset($aboutPage->foreground_image) }}" class="img-fluid" alt="">
+                <a href="{{ $aboutPage->video_url }}" class="glightbox pulsating-play-btn"></a>
             </div>
 
             <div class="col-lg-6 content order-last  order-lg-first" data-aos="fade-up" data-aos-delay="100">
-                <h3>{{$aboutPage ->title}}</h3>
+                <h3>{{ $aboutPage->title }}</h3>
                 <p>
-                    {{$aboutPage->sub_title}}
+                    {{ $aboutPage->sub_title }}
                 </p>
-                
+
                 <ul>
 
                     @foreach ($aboutItems as $item)
-                       <li>
-                        <i class="{{$item->item_icon}}"></i>
-                        <div>
-                            <h5>{{$item->item_title}}</h5>
-                            <p>{{$item->item_description}}</p>
-                        </div>
-                    </li> 
+                        <li>
+                            <i class="{{ $item->item_icon }}"></i>
+                            <div>
+                                <h5>{{ $item->item_title }}</h5>
+                                <p>{{ $item->item_description }}</p>
+                            </div>
+                        </li>
                     @endforeach
-                    
-                    
+
+
                 </ul>
             </div>
 
